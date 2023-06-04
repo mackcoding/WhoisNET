8e786970961d40ca1098042d1afbcd90afea3b20
Thomas Mack <mack@mackcoding.com>
2023-06-03 21:28:44 -0400

    Updated Program.cs for Whois.NET_Client

--- Examples/Program.cs
+++ Examples/Program.cs
@@ -11,33 +11,8 @@ namespace Whois.NET_Client
     {
         static void Main(string[] args)
         {
-            var Lines = File.ReadAllLines("domains.txt");
-            List<string> Result = new List<string>();
-
-            //foreach (var Line in Lines)
-           // {
-            //    Console.WriteLine($"{Line}: {GetAbuse(QueryTools.GetWhois(Line))}");
-           // }
-
             Console.WriteLine(QueryTools.GetWhois("8.8.8.8"));
-            //Console.WriteLine(QueryTools.GetWhois("https://google.com"));
-            //Console.WriteLine(QueryTools.GetWhois("generates.pw"));
-            //Console.WriteLine(QueryTools.GetWhois("sre.google"));
-            Console.ReadLine();
         }
-
-        static string GetAbuse(string Whois)
-        {
-            string Pattern = @".* Abuse .* ([\w.-]+@[\w.-]+\.\w+)";
-            Match ServerMatch = Regex.Match(Whois, Pattern);
-
-            if (ServerMatch.Success)
-                return ServerMatch.Groups[1].Value;
-            else
-                return string.Empty;
-
-        }
-
         
     }
 }
