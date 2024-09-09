3aae02740515abac0d5eb71ec9572d7aa64dcd10
Thomas Mack <mack@mackcoding.com>
2024-09-08 21:53:43 -0400

    chore: fix dash isssue in tokenizer
    fixes #4
    - added 'previous'
    - isDashInToken returns true or false depending on specific conditions
    - fixes all tests, all tests now pass
    - rewrote TestMixedOptions

--- src/WhoisNET.Tests/TokenizerTests.cs
+++ src/WhoisNET.Tests/TokenizerTests.cs
@@ -73,8 +73,23 @@ namespace WhoisNET.Tests
             var input = "-h example.com --port 80 -d --no-recursion query.com";
             var result = Tokenizer.Tokenize(input);
 
-            Assert.Throws<InvalidOperationException>(() => Tokenizer.Tokenize(input));
+            Assert.Multiple(() =>
+            {
+                Assert.That(result, Is.Not.Null);
+                Assert.That(result, Has.Count.EqualTo(5));
+                Assert.That(result, Contains.Key(OptionEnum.host));
+                Assert.That(result[OptionEnum.host], Is.EqualTo("example.com"));
+                Assert.That(result, Contains.Key(OptionEnum.port));
+                Assert.That(result[OptionEnum.port], Is.EqualTo(80));
+                Assert.That(result, Contains.Key(OptionEnum.debug));
+                Assert.That(result[OptionEnum.debug], Is.EqualTo(true));
+                Assert.That(result, Contains.Key(OptionEnum.no_recursion));
+                Assert.That(result[OptionEnum.no_recursion], Is.EqualTo(true));
+                Assert.That(result, Contains.Key(OptionEnum.query));
+                Assert.That(result[OptionEnum.query], Is.EqualTo("query.com"));
+            });
         }
+
 
         [Test]
         public void TestHelpOption()
