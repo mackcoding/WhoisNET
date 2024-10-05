0e147bbb733d52d2f9b764c91953f50afc2fe908
Thomas Mack <mack@mackcoding.com>
2024-10-05 00:14:03 -0400

    chore: resolve problematic domain ips
    - Write a proper parser that can handle finding the referral without regex
    
    fixes #13

--- src/WhoisNET.Client/Program.cs
+++ src/WhoisNET.Client/Program.cs
@@ -1,14 +1,72 @@
-﻿using WhoisNET;
+﻿using System.Diagnostics;
+using WhoisNET;
 using WhoisNET.Client.CmdOptions;
 using WhoisNET.Enums;
+using WhoisNET.Parser;
 
+string text = @"% IANA WHOIS server
+% for more information on IANA, visit http://www.iana.org
+% This query returned 1 object
+
+refer:        whois.arin.net
+
+inetnum:      128.0.0.0 - 128.255.255.255
+organisation: Administered by ARIN
+status:       LEGACY
+
+whois:        whois.arin.net
+
+changed:      1993-05
+source:       IANA";
+
+
+
+
+
+string text2 =@"%rwhois V-1.5:003eff:00 rwhois.centrilogic.com (by Network Solutions, Inc. V-1.5.9.5)
+%referral rwhois://root.rwhois.net:4321/auth-area=.
+%ok";
+
+Stopwatch sw = new Stopwatch();
+
+sw.Start();
+{
+    ReferralParser.GetReferral(text2);
+}
+sw.Stop();
+
+Console.WriteLine($"Execution Time: {sw.ElapsedMilliseconds} ms");
+
+sw.Start();
+{
+    ReferralParser.GetReferral(text);
+}
+sw.Stop();
+
+Console.WriteLine($"Execution Time: {sw.ElapsedMilliseconds} ms");
+
+sw.Start();
+{
+    Utilities.GetReferral(text);
+}
+sw.Stop();
+
+Console.WriteLine($"Execution Time: {sw.ElapsedMilliseconds} ms");
+
+sw.Start();
+{
+    Utilities.GetReferral(text2);
+}
+sw.Stop();
+
+Console.WriteLine($"Execution Time: {sw.ElapsedMilliseconds} ms");
 
 
 
 try
 {
-    //var options = Tokenizer.Tokenize("--verbose mackcoding.com");
-    var options = Tokenizer.Tokenize(string.Join(' ', args));
+    var options = Tokenizer.Tokenize("--verbose 128.14.219.125");
+    //var options = Tokenizer.Tokenize(string.Join(' ', args));
 
     Dictionary<QueryOptions, object> queryOptions = [];
 
---
+++ src/WhoisNET/Parser/ReferralParser.cs
@@ -0,0 +1,99 @@
+﻿using System.Text;
+
+namespace WhoisNET.Parser
+{
+    /// <summary>
+    /// Searches the whois information for a referral server.
+    /// This is essential a lightweight version of the WhoisParser.
+    /// </summary>
+    public static class ReferralParser
+    {
+        private static readonly List<string> _keywords = [
+            "refer",
+            "whois",
+            "referralserver",
+            "referral",
+            "referer"
+        ];
+
+
+        /// <summary>
+        /// Searches whois data for a referral server.
+        /// </summary>
+        /// <param name="data">Whois data to search</param>
+        /// <returns>Hostname and port of referral</returns>
+        public static (string host, int port) GetReferral(string data)
+        {
+            using StringReader reader = new(data);
+
+            string? line;
+            StringBuilder host = new(),
+                port = new();
+
+            while ((line = reader.ReadLine()) != null)
+            {
+                if (line.StartsWith('%'))
+                    line = line[1..];
+
+                string? keyword = FindKeyword(line);
+
+                if (!string.IsNullOrEmpty(keyword))
+                {
+                    bool bFoundColon = false;
+                    line = line[keyword.Length..].TrimStart(':').Trim();
+
+                    line = line.Trim();
+
+                    for (int i = 0; i < line.Length; i++)
+                    {
+                        char c = line[i];
+
+                        if (char.IsWhiteSpace(c))
+                            continue;
+
+                        if (c == '/')
+                            break;
+
+                        if (c == ':')
+                        {
+                            bFoundColon = true;
+                            continue;
+                        }
+
+                        if (bFoundColon)
+                            port.Append(c);
+                        else
+                            host.Append(c);
+                    }
+                    break;
+                }
+            }
+
+
+            if (!int.TryParse(port.ToString(), out int portnum))
+                portnum = 53;
+
+                return (host.ToString(), portnum);
+        }
+
+        /// <summary>
+        /// Returns a keyword match
+        /// </summary>
+        /// <param name="line">Line of text to match</param>
+        /// <returns>Matched keyword</returns>
+        private static string FindKeyword(string line)
+        {
+            foreach (var keyword in _keywords)
+            {
+                if (line.StartsWith(keyword, StringComparison.OrdinalIgnoreCase) &&
+                    (line.Length == keyword.Length || !char.IsLetterOrDigit(line[keyword.Length])))
+                    return keyword;
+
+            }
+
+            return string.Empty;
+        }
+
+
+    }
+}
